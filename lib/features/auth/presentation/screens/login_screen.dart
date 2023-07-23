82c791b44c0cca5c9a42cc2317b8022994034028
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-07-23 18:59:49 +0200

    Add geometrical background

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -13,29 +13,31 @@ class LoginScreen extends StatelessWidget {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
-        body: SingleChildScrollView(
-          physics: const ClampingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 80),
-              const Icon(
-                Icons.production_quantity_limits_rounded,
-                color: Colors.white,
-                size: 100,
-              ),
-              const SizedBox(height: 80),
-              Container(
-                height: size.height - 260,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: scaffoldBackgroundColor,
-                  borderRadius:
-                      const BorderRadius.only(topLeft: Radius.circular(100)),
+        body: GeometricalBackground(
+          child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 80),
+                const Icon(
+                  Icons.production_quantity_limits_rounded,
+                  color: Colors.white,
+                  size: 100,
                 ),
-                child: const _LoginForm(),
-              )
-            ],
+                const SizedBox(height: 80),
+                Container(
+                  height: size.height - 260,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: scaffoldBackgroundColor,
+                    borderRadius:
+                        const BorderRadius.only(topLeft: Radius.circular(100)),
+                  ),
+                  child: const _LoginForm(),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:teslo_app/features/shared/shared.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child:
+          Scaffold(body: GeometricalBackground(child: SingleChildScrollView())),
+    );
   }
 }
