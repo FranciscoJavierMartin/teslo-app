abc9d0ef721a8e3e17bc21a7cc656c900d83591a
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-07-23 14:02:49 +0200

    Custom text form field

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:teslo_app/features/shared/widgets/custom_text_form_field.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    final scaffoldBackgroundColor = Theme.of(context).scaffoldBackgroundColor;
+
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 80),
+              const Icon(
+                Icons.production_quantity_limits_rounded,
+                color: Colors.white,
+                size: 100,
+              ),
+              const SizedBox(height: 80),
+              Container(
+                height: size.height - 260,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: scaffoldBackgroundColor,
+                  borderRadius:
+                      const BorderRadius.only(topLeft: Radius.circular(100)),
+                ),
+                child: const _LoginForm(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm();
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          const SizedBox(height: 50),
+          Text('Login', style: textStyles.titleLarge),
+          const SizedBox(height: 90),
+          const CustomTextFormField(
+            label: 'Email',
+            keyboardType: TextInputType.emailAddress,
+          ),
+        ],
+      ),
+    );
   }
 }
