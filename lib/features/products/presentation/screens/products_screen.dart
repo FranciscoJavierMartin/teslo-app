ae17fb1aa91ccd3222ad01fc7f9d127f436b5143
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-07-23 19:47:42 +0200

    Product page

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,10 +1,39 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:teslo_app/features/shared/shared.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return Scaffold(
+      drawer: SideMenu(scaffoldKey: scaffoldKey),
+      appBar: AppBar(
+        title: const Text('Products'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search_rounded),
+          )
+        ],
+      ),
+      body: const _ProductsView(),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text('New product'),
+        icon: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ProductsView extends StatelessWidget {
+  const _ProductsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Hello world'));
   }
 }
